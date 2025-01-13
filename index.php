1e795273ef126d2c9189ffa4d6f3e52cb98b379e
Jelle Wijma <jelle.wijma@gmail.com>
2025-01-13 10:17:21 +0100

    nav moved to a component

--- index.php
+++ index.php
@@ -10,21 +10,7 @@
     <title>CutOrDye</title>
 </head>
 <body>
-    <nav  class="navbar" role="navigation" aria-label="main navigation" >
-        <div id="navbarBasicExample" class="navbar-menu" style="background-color: #C4C4C4">
-            <div class="navbar-start" >
-                <a class="navbar-item" href="#">
-                    <img  src="./fotos/logo_CutOrDye.png" alt="logo"/>
-                </a>
-            </div>
-            <div class="navbar-end" >
-                    <a class="column" href="./login.php" >login</a>
-                    <a class="column" href="#">about</a>
-                    <a class="column" href="./makeReservation.php">reserveren</a>
-                    <a class="column is-one-third" href="./account.php">my account</a>
-            </div>
-        </div>
-    </nav>
+   <?php include './components/nav.php'?>
     <main>
         <section class=" mg-large section is-flex" style="justify-content: center">
             <div class="foto-container is-flex-direction-row is-3" style="padding: max(50px) justify-content: space-between" >
